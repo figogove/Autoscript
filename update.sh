fa218703202bac8a91a2349b1c73ffbcf6fec1ef
figogove <102362899+figogove@users.noreply.github.com>
2024-02-07 18:03:16 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Gemilangkinasih࿐
-# Email      » gemilangkinasih@gmail.com
-# telegram   » https://t.me/gemilangkinasih
-# whatsapp   » wa.me/+628984880039
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# Gemilangkinasih࿐
+#
 
 clear
 fun_bar() {
@@ -36,7 +29,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Baimkuy/Autoscript/GM/menu/menu.zip
+    wget https://raw.githubusercontent.com/figogove/Autoscript/GM/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
