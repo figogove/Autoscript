becda3413a88817f9a91de5711d55c5c01992a37
figogove <102362899+figogove@users.noreply.github.com>
2024-02-15 13:22:18 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,11 +1,5 @@
 #!/bin/bash# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Gemilangkinasih࿐
-# Email      » gemilangkinasih@gmail.com
-# telegram   » https://t.me/gemilangkinasih
-# whatsapp   » wa.me/+628984880039
-# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# Gemilangkinasih࿐
+#
 
 #random
 apt install jq curl -y
